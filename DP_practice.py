fb96c964b1dc4cfe04dfd085f67de8c0f81fa11f
mendezja <mendez-anastasioj24@mail.wlu.edu>
2023-09-11 14:58:18 -0400

    Finalized dp practice

--- DP_practice.py
+++ DP_practice.py
@@ -11,31 +11,22 @@
 def waterTrapped(heights):
     size = len(heights)
     
-    left_max = [-1] * size
-    left_max[-1] = heights[-1]
+    left_max = list(height)
+    right_max = list(height)
 
-    right_max = [-1] * size
-    right_max[0] = heights[0]
     
-    # print(right_max)
-    # print(left_max)
-    
-    for x in range(1,size):
-        right_max[x] = max(right_max[x-1], heights[x])
+    for i in range(1,size):
+        right_max[i] = max(right_max[i-1], heights[i])
 
-        l_x = (size-1) - x
-        left_max[l_x] = max(left_max[l_x +1], heights[l_x])
-    print(heights)
-    print(right_max)
-    print(left_max)
-        # print()
-    water_sum = 0
-    for x in range(size):
-        water_sum += min(right_max[x]-heights[x], left_max[x]-heights[x])
-    return water_sum
+        l_i = (size-1) - i
+
+        left_max[l_i] = max(left_max[l_i +1], heights[l_i])
+    
+    return sum(map(lambda x,y,z: min(x-z, y-z), right_max, left_max, height))
+
 
 
 height = [0,1,0,2,1,0,1,3,2,1,2,1]
 ans = [0,0,1,0,1,2,1,0,0,1,0,0]
 print(waterTrapped(height))
-print(ans)
+# print(ans)
