866dea353ea52fbf624228b5c2b47ec9f0845368
mendezja <mendez-anastasioj24@mail.wlu.edu>
2023-09-29 12:13:43 -0400

    comments, minor edits

--- Assignment 2/TreasureMDP.py
+++ Assignment 2/TreasureMDP.py
@@ -3,6 +3,9 @@
 
 import random
 
+
+# State Transition Matrix
+# Last s ommited given no possible s'
 STM =  [[0.0, 0.6, 0.1, 0.3, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0] ,
         [0.0, 0.0, 0.0, 0.0, 0.3, 0.7, 0.0, 0.0, 0.0, 0.0, 0.0] ,
         [0.0, 0.0, 0.0, 0.0, 0.0, 0.2, 0.8, 0.0, 0.0, 0.0, 0.0] ,
@@ -14,8 +17,8 @@ STM =  [[0.0, 0.6, 0.1, 0.3, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0] ,
         [0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.0, 0.0, 0.0, 0.0] ,
         [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 0.0, 0.5]]
 
-# 0 indicates no treaure, 2 indicates treaure, 5 indicates terminal state
 # States labeled by index
+# key: 0 indicates no treaure, 2 indicates treaure, 5 indicates terminal state
 STATES = [0,0,0,2,2,0,2,0,0,0,5]
 
 GAMMA = 0.9
@@ -24,7 +27,7 @@ ACTIONS = ["dig", "move"]
 
 
 class Agent():
-    
+
     def __init__(self):
         self.loc = 0
         self.timeStep = 0
@@ -41,7 +44,7 @@ class Agent():
         # Dig
         if action == 0:
 
-            # check for treasure and update attributes
+            # Check for treasure and update attributes
             if STATES[self.loc] == 2 :
                 self.t_found += 1
                 self.award(2) 
@@ -53,25 +56,21 @@ class Agent():
             # Update reward
             self.award(-1)
 
+            # Update Location
             self.updateLocation()
             
             # Check if terminal state
             if STATES[self.loc] == 5: 
-
-                # Update Reward accordening
                 self.award( 20 if self.t_found == 3 else 5)
 
-                # return "{:2d}. Reward: {:.2f} | State: Terminal | Action: {}".format(self.timeStep, self.reward, ACTIONS[action])
-
-
-        # Return stats after action
-        stepReport = "{:2d}. Reward: {:.2f} | State: {} | Action: {}"
-        return stepReport.format(self.timeStep, self.reward, "T" if self.loc == 10 else self.loc, ACTIONS[action])
+        # Return stats after action 
+        return (self.timeStep, self.reward, "T" if self.loc == 10 else self.loc, ACTIONS[action])
 
 
 
     def award(self, quantity):
         self.reward += (GAMMA ** self.timeStep) * quantity
+
 
     def updateLocation(self):
         current = self.loc
@@ -94,19 +93,29 @@ class Agent():
 
 
 def main():
-    # Store episodes history
+
+    # Store episodes data
     history = [[] for _ in range(10)]
 
+    stepReport = "{:2d}. Reward: {:.2f} | State: {} | Action: {}"
+
+    # 10 episodes
     for i in range(10):
 
         agent = Agent()
-        print("\nEpisode ", i + 1)
-        print(" 0. Reward:  0.00 | State: 0 | Action: None")
 
+        print("\nEpisode ", i + 1)
+        print(stepReport.format(0, 0.00, "I", "null" ))
+
+
+        # 25 steps or till terminal reached
         while agent.timeStep < 25 and agent.loc !=10:
-            action = agent.act()
-            print(action)
-            history[i].append(action) 
+
+            data = agent.act()
+            history[i].append(data) 
+
+            print(stepReport.format(data))
+
         print("Cumulative Reward: {:.2f}".format(agent.reward))
     
 
